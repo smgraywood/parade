f793d6da555ac49dadb04c3afdd786a4358be070
Sarah Graywood <smgraywood@gmail.com>
2024-03-20 15:07:15 -0700

    step construction updates

--- parade/models.py
+++ parade/models.py
@@ -1,24 +1,54 @@
-from dataclasses import dataclass
-from typing import Optional
+"""establish class model and calculate free float and total float"""
 
-@dataclass
-class Step: 
+import functools
+from dataclasses import dataclass
+from typing import Annotated
+
+from annotated_types import Ge
+
+PositiveInt = Annotated[int, Ge(0)]
+
+
+@dataclass(frozen=True)
+class Step:
+    """describe one task in a series"""
+
     name: str
-    duration: int
-    total_float: int
-    free_float: int
-    es: int
-    ls: int
-    ef: int
-    lf: int
+    duration: PositiveInt
+    es: PositiveInt
+    ls: PositiveInt
+    ef: PositiveInt
+    lf: PositiveInt
     children: list["Step"]
     parents: list["Step"]
-    
 
-def total_float_calc(step:Step)->int:
-    return step.ls - step.es 
-#sometimes calc by lf-ef instead
+    @functools.cached_property
+    def total_float(self) -> PositiveInt:
+        """Amount of time task can be delayed w/o affecting completion date of project.
 
-def free_float_calc(step:Step)->int:
-    return min([child.es for child in step.children])
+        Returns:
+            PositiveInt: Time in project units
+        """
+        return total_float_calc(self)
 
+    @functools.cached_property
+    def free_float(self) -> PositiveInt:
+        """Amount of time activity can be delayed w/o delaying start of successor.
+
+        Returns:
+            PositiveInt: Time in project units.
+        """
+        return free_float_calc(self)
+
+
+def total_float_calc(step: Step) -> int:
+    if step.ls < 0 or step.es < 0:
+        raise ValueError("please only use positive numbers")
+    return step.ls - step.es
+    # sometimes calc by lf-ef instead
+
+
+def free_float_calc(step: Step) -> int:
+    if step.children:
+        return min(child.es for child in step.children)
+    return 0
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,20 +1,40 @@
-#happy, sad, bad, mad
-
-#happy
-#happy would be if we can calc the total and free float correctly using the given info
-
-#sad
-#can't calc free and total float, or get an error
-
-#bad
-#can calc a random thing or give random things to calc that is not correct
-
-#mad
+"""Tests for core models."""
 
 import hypothesis.strategies as st
 from hypothesis import given
-from parade.models import Step, total_float_calc
+
+from parade.models import Step, free_float_calc, total_float_calc
+
 
 @given(st.builds(Step))
-def test_total_float_calc(step:Step):
-    assert total_float_calc(step) <= step.ls 
+def test_total_float_calc(step: Step):
+    # assume(step.ls >= 0 and step.es >= 0)
+    assert total_float_calc(step) <= step.ls
+
+
+def test_known_total_float_calc():
+    step = Step(
+        name="Activity 6",
+        duration=4,
+        es=1,
+        ls=2,
+        ef=1,
+        lf=5,
+        children=[],
+        parents=[],
+    )
+    assert total_float_calc(step) == 1
+
+
+@given(st.builds(Step))
+def test_free_float_calc(step: Step):
+    if step.children:
+        assert free_float_calc(step) == min(child.es for child in step.children)
+    else:
+        assert free_float_calc(step) == 0
+
+
+@given(st.builds(Step))
+def test_step_props(step: Step):
+    assert step.free_float == free_float_calc(step)
+    assert step.total_float == total_float_calc(step)
